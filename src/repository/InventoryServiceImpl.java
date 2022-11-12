2d6a75d3f5ed6874c849877687307102bd63b5fa
felixa <felixarajiph@gmail.com>
2022-11-12 23:36:25 +0700

    [FEAT] Add ui & fix bug

--- src/repository/InventoryServiceImpl.java
+++ src/repository/InventoryServiceImpl.java
@@ -6,7 +6,8 @@ import responses.SuccessStatus;
 import utils.exceptions.EmptyListException;
 import utils.exceptions.ValidationException;
 
-import java.io.*;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -18,7 +19,7 @@ import java.util.stream.Collectors;
 
 public class InventoryServiceImpl implements InventoryService {
     private final ArrayList<Book> books;
-    private Path file;
+    private final Path file;
 
     public InventoryServiceImpl(ArrayList<Book> books) {
         this.books = books;
@@ -37,7 +38,7 @@ public class InventoryServiceImpl implements InventoryService {
         String id = metadata[1];
         String publishedYear = metadata[2];
         String writterName = metadata[3];
-        return new Book(title, id, publishedYear, publishedYear);
+        return new Book(title, publishedYear, publishedYear);
     }
 
     @Override
@@ -47,7 +48,7 @@ public class InventoryServiceImpl implements InventoryService {
         }
         books.add(book);
         try {
-            var results = book.getTitle() +"||" +book.getId()+"||" + book.getPublishedYear()+"||" + book.getWriterName() + "\n";
+            var results = book.getTitle() + "||" + book.getId() + "||" + book.getPublishedYear() + "||" + book.getWriterName() + "\n";
             Files.write(file, results.getBytes(StandardCharsets.UTF_8), StandardOpenOption.CREATE, StandardOpenOption.APPEND);
         } catch (IOException err) {
             System.out.println(err.getMessage());
@@ -57,16 +58,27 @@ public class InventoryServiceImpl implements InventoryService {
     }
 
     @Override
-    public Responses delete(Integer id) {
-        if (getAll() == null) {
-            throw new ValidationException();
+    public Responses delete() {
+        try {
+            var results = Files.lines(file).collect(Collectors.toList());
+            var size = results.size();
+            if (size < 1) {
+                throw new EmptyListException();
+            }
+            FileWriter writer = new FileWriter(file.toFile());
+            results.remove(size - 1);
+            for (var str : results) {
+                writer.write(str + "||" + "\n");
+            }
+            writer.close();
+        } catch (IOException err) {
+            err.printStackTrace();
         }
-        books.remove(id);
         return new SuccessStatus("Delete success");
     }
 
     @Override
-    public Book searchByid(Integer id) {
+    public Book searchByid(String id) {
         var results = books.stream().filter(book -> book.getId().equals(id)).collect(Collectors.toList());
         if (results.size() < 1) {
             throw new EmptyListException();
@@ -77,17 +89,10 @@ public class InventoryServiceImpl implements InventoryService {
     @Override
     public List<Book> getAll() {
         List<Book> lines = null;
-//        try (BufferedReader br = Files.newBufferedReader(file)) {
-//            String line = br.readLine();
-//            while (line != null) {
-//                String[] attributes = line.split("\\|\\|");
-//                Book book1 = createBook(attributes);
-//                lines.add(book1);
-//            }
-        try{
-            lines = Files.lines(file).map(member->{
-                String [] arr = member.split("\\|\\|");
-                return new Book(arr[0],arr[1],arr[2],arr[3]);
+        try {
+            lines = Files.lines(file).map(member -> {
+                String[] arr = member.split("\\|\\|");
+                return new Book(arr[0], arr[2], arr[3]);
             }).collect(Collectors.toList());
         } catch (IOException err) {
             err.printStackTrace();
@@ -98,7 +103,7 @@ public class InventoryServiceImpl implements InventoryService {
     @Override
     public List<Book> searchByTitle(String title) {
 
-        var results = books.stream().filter(book -> book.getTitle().toLowerCase().equals(title.toLowerCase())).collect(Collectors.toList());
+        var results = books.stream().filter(book -> book.getTitle().equalsIgnoreCase(title)).collect(Collectors.toList());
         if (results.size() < 0) {
             throw new EmptyListException();
         }
